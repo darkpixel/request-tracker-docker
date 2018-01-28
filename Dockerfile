d5c992e62b566173944296baf87cc7ac6c84ed52
Aaron C. de Bruyn <aaron@heyaaron.com>
2018-01-28 15:49:42 -0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM darkpixel/request-tracker-docker-base:dev
+LABEL maintainer="Aaron C. de Bruyn <aaron@heyaaron.com>"
+
+WORKDIR /opt/src
+RUN curl -sLS "https://download.bestpractical.com/pub/rt/release/rt-4.4.2.tar.gz" | tar --strip-components=1 -xvzf - \
+&& ./configure --enable-graphviz --enable-gd --with-db-type=Pg --with-db-host=database \
+&& make testdeps && make install
+
+RUN PERL_MM_USE_DEFAULT=1 cpan install \
+RT::Extension::Announce \
+RT::Extension::Gravatar \
+RT::Extension::MergeUsers \
+RT::Extension::QuickUpdate \
+RT::Extension::RepeatTicket \
+RT::Extension::ResetPassword \
+#RT::Extension::REST2 \
+RT::Extension::TicketLocking \
+RT::Extension::BounceEmail \
+RT::Action::SetPriorityFromHeader \
+&& rm -rf /root/.cpan
+
+RUN apk add --update gettext
+
+WORKDIR /opt/rt4
+COPY RT_SiteConfig.pm ./etc/RT_SiteConfig.pm.template
+EXPOSE 80
+CMD envsubst '\$DATABASE_HOST \$DATABASE_USER \$DATABASE_PASSWORD \$RT_NAME \$OWNER_EMAIL \$WEB_DOMAIN \$WEB_BASE_URL \
+  \$CORRESPOND_ADDRESS \$COMMENT_ADDRESS \$DATABASE_PORT \$WEB_PORT \$LOGO_URL \$LOGO_LINK_URL \$TIMEZONE \
+  \$WEB_SECURE_COOKIES' \
+  < /opt/rt4/etc/RT_SiteConfig.pm.template \
+  > /opt/rt4/etc/RT_SiteConfig.pm \
+  && /opt/rt4/sbin/rt-server --port 80
---
+++ RT_SiteConfig.pm
@@ -0,0 +1,207 @@
+use utf8;
+
+# Set($SendmailPath , "/opt/rt4/etc/msmtp_wrapper");
+
+Set($DatabaseType, "Pg");
+Set($DatabaseHost,   "${DATABASE_HOST}");
+Set($DatabasePort, "${DATABASE_PORT}");
+Set($DatabaseUser, "${DATABASE_USER}");
+Set($DatabasePassword, q{${DATABASE_PASSWORD}});
+Set($DatabaseName, q{support});
+#Set(%DatabaseExtraDSN, sslmode => 'require' );
+
+Set($rtname, '${RT_NAME}');
+Set($OwnerEmail, '${OWNER_EMAIL}');
+Set($Organization, '${RT_NAME}');
+Set($WebDomain, '${WEB_DOMAIN}');
+Set($Timezone, '${TIMEZONE}');
+Set($WebPort, '${WEB_PORT}');
+Set($WebBaseURL, '${WEB_BASE_URL}');
+#Set($WebSecureCookies, 1);
+Set($CanonicalizeRedirectURLs, 1);
+Set($CorrespondAddress, '${CORRESPOND_ADDRESS}');
+Set($CommentAddress, '${COMMENT_ADDRESS}');
+Set($TimeInICal, 1);
+Set($ShowUnreadMessageNotifications, 1);
+
+Set($HTMLFormatter, 'w3m');
+
+Set($LogoURL, '${LOGO_URL}');
+Set($LogoLinkURL, '${LOGO_LINK_URL}');
+
+Set( %FullTextSearch,
+    Enable     => 1,
+    Indexed    => 0,
+    Column     => 'ContentIndex',
+    Table      => 'Attachments',
+);
+
+# Plugin('RT::Extension::ActivityReports');
+# Plugin('RT::Extension::AdminConditionsAndActions');
+
+Plugin('RT::Extension::Announce');
+Set(@CustomFieldValuesSources, (qw(RT::CustomFieldValues::AnnounceGroups)));
+
+Plugin('RT::Extension::Gravatar');
+Plugin('RT::Extension::MergeUsers');
+#Plugin('RT::Extension::QuickAssign');
+Plugin('RT::Extension::QuickUpdate');
+Plugin('RT::Extension::RepeatTicket');
+#Plugin('RT::Extension::RepliesToResolved');
+Plugin('RT::Extension::ResetPassword');
+#Plugin('RT::Extension::REST2');
+Plugin('RT::Extension::TicketLocking');
+Plugin('RT::Extension::BounceEmail');
+Plugin('RT::Action::SetPriorityFromHeader');
+
+Set($PriorityHeader, 'X-Priority');
+Set(%PriorityMap, highest => 1, high => 2, normal => 3, low => 4, lowest => 5);
+
+Set( $LockExpiry, 5*60 );
+Set($HomepageComponents, [qw(
+    MyLocks
+    MyReminders
+    QueueList
+    Dashboards
+    FindUser
+    SavedSearches
+    QuickCreate
+    RefreshHomepage
+)]);
+
+%RT::ServiceAgreements = (
+    AssumeOutsideActor => 1,
+    Default => 'normal',
+    QueueDefault => {
+        'emergency' => 'emergency',
+    },
+    Levels => {
+        'emergency'  => {
+          StartImmediately => 1,
+          Response => { RealMinutes => 15 },
+          KeepInLoop => { RealMinutes => 30 },
+          Resolve  => { RealMinutes => 60*4 },
+        },
+        'normal'  => {
+          Response => { BusinessMinutes => 60*8 },
+          Resolve => { BusinessMinutes => 60*24*7 },
+        },
+        'maintenance' => {
+          Response => { BusinessMinutes => 60*24*7 },
+          Response => { BusinessMinutes => 60*24*30 },
+        },
+    },
+);
+%RT::ServiceBusinessHours = (
+    'standard' => {
+        0 => { Name => 'Sunday', Start => 'undef', End => 'undef' },
+        1 => { Name => 'Monday', Start => '7:00', End => '18:00' },
+        2 => { Name => 'Tuesday', Start => '7:00', End => '18:00' },
+        3 => { Name => 'Wednesday', Start => '7:00', End => '18:00' },
+        4 => { Name => 'Thursday', Start => '7:00', End => '18:00' },
+        5 => { Name => 'Friday', Start => '7:00', End => '15:00' },
+        6 => { Name => 'Saturday', Start => 'undef', End => 'undef' },
+    },
+);
+
+Set(%Lifecycles,
+    default => {
+        initial         => [qw(new)], # loc_qw
+        active          => [qw(open scheduled on-hold)], # loc_qw
+        inactive        => [qw(closed rejected deleted)], # loc_qw
+
+        defaults => {
+            on_create => 'new',
+            on_merge  => 'closed',
+            approved  => 'open',
+            denied    => 'rejected',
+            reminder_on_open     => 'open',
+            reminder_on_resolve  => 'closed',
+        },
+        transitions => {
+            ''                  => [qw(new open scheduled on-hold closed rejected deleted)],
+            'new'               => [qw(    open scheduled on-hold closed rejected deleted)],
+            'open'              => [qw(new      scheduled on-hold closed rejected deleted)],
+            'scheduled'         => [qw(new open           on-hold closed rejected deleted)],
+            'on-hold'           => [qw(new open scheduled         closed rejected deleted)],
+            'closed'            => [qw(new open scheduled on-hold        rejected deleted)],
+            'rejected'          => [qw(new open scheduled on-hold closed          deleted)],
+            'deleted'           => [qw(new open scheduled on-hold closed rejected        )],
+        },
+        rights => {
+            '* -> deleted'  => 'DeleteTicket',
+            '* -> *'        => 'ModifyTicket',
+        },
+        actions => [
+            'new -> open'                   => { label  => 'Take',                 update => 'Respond' }, # loc{label}
+            'new -> closed'                 => { label  => 'Close',                update => 'Respond' }, # loc{label}
+            'new -> deleted'                => { label  => 'Delete',                                   }, # loc{label}
+            'new -> rejected'               => { label  => 'Reject',               update => 'Respond' }, # loc{label}
+            'open -> closed'                => { label  => 'Close',                update => 'Respond' }, # loc{label}
+            'open -> on-hold'               => { label  => 'Hold',                 update => 'Respond' }, # loc{label}
+            'closed -> open'                => { label  => 'Re-open',              update => 'Respond' }, # loc{label}
+            'deleted -> open'               => { label  => 'Undelete',                                 }, # loc{label}
+        ],
+    },
+    assets => {
+        type     => "asset",
+        initial  => [
+            'new'
+        ],
+        active   => [
+            'allocated',
+            'in-use',
+            'bone-yard'
+        ],
+        inactive => [
+            'dead',
+            'recycled',
+            'stolen',
+            'deleted',
+            'destroyed',
+            'rma'
+        ],
+
+        defaults => {
+            on_create => 'new',
+        },
+
+        transitions => {
+            ''         => [qw(new allocated in-use rma dead)],
+            new        => [qw(allocated in-use stolen bone-yard rma dead)],
+            allocated  => [qw(in-use recycled stolen destroyed bone-yard rma dead)],
+            "in-use"   => [qw(allocated recycled stolen bone-yard rma dead)],
+            recycled   => [qw(allocated bone-yard rma dead)],
+            destroyed  => [qw(deleted)],
+            stolen     => [qw(deleted)],
+            rma        => [qw(deleted)],
+            dead       => [qw(deleted)],
+        },
+        rights => {
+            '* -> *'        => 'ModifyAsset',
+        },
+        actions => {
+            '* -> allocated' => {
+                label => "Allocate"
+            },
+            '* -> in-use'    => {
+                label => "Now in-use"
+            },
+            '* -> bone-yard'  => {
+                label => "Bone Yard"
+            },
+            '* -> stolen'    => {
+                label => "Report stolen"
+            },
+            '* -> destroyed'    => {
+                label => "Destroyed"
+            },
+            '* -> rma'    => {
+                label => "RMAd"
+            },
+        },
+    },
+);
+
+1;
+
